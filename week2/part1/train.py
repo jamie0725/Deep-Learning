1caaeec6906945411cb799741cec5719321fad9b
Kai Liang <k.liang.2@student.rug.nl>
2019-04-28 17:15:38 +0200

    finish part 1

--- week2/part1/train.py
+++ week2/part1/train.py
@@ -82,12 +82,12 @@ def train(config):
     # Initialize the model that we are going to use
     if config.model_type == 'RNN':
         model = VanillaRNN(config.input_length, config.input_dim, 
-                            config.num_hidden, config.num_classes,
-                            config.batch_size, device)
+                           config.num_hidden, config.num_classes,
+                           config.batch_size, device)
     else:
         model = LSTM(config.input_length, config.input_dim, 
-                      config.num_hidden, config.num_classes,
-                      config.batch_size, device)
+                     config.num_hidden, config.num_classes,
+                     config.batch_size, device)
     model.to(device)
 
     # Initialize the dataset and data loader (note the +1)
